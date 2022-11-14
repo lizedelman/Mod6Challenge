4ea909a80022af63cb73597b9ee311c33d842be7
Wild Carrot <edelman.eliz@gmail.com>
2022-11-14 13:38:46 -0700

    hide api key

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-var APIKey = "6561d55b38895db2712787639eb1bdd9";
+var config = config.APIKey;
 var searchinput = document.getElementById("searchcityinput");
 var btnsearch = document.getElementById("button-addon2");
-var lat;
-var lon;
+var cityname = document.querySelector(".cityname");
+var citytemp = document.querySelector(".citytemp");
+var citywind = document.querySelector(".citywind");
+var cityhumidity = document.querySelector(".cityhumidty");
 
 btnsearch.addEventListener("click", click);
 function click() {
@@ -13,29 +15,6 @@ function click() {
   getApi(city);
   console.log(city);
 }
-
-//lat & long to get: reqeust to url to get lat & lon and then run another reuqest to get the weather
-var convertLatLon =
-  "http://api.openweathermap.org/geo/1.0/direct?q=" +
-  lat +
-  "&" +
-  lon +
-  "&appid=" +
-  APIKey;
-
-function getApi(latLon) {
-  console.log(latLon);
-  fetch(convertLatLon).then(function (response) {
-    if (response.ok) {
-      var city = response.json().then(function (data) {
-        console.log(data);
-        getApi(city);
-      });
-    }
-  });
-}
-
-//Not working correctly
 
 function getApi(city) {
   console.log(city);
@@ -52,12 +31,48 @@ function getApi(city) {
     })
     .then(function (data) {
       console.log(data);
-      var currentdaysdata = document.getElementById("currentdaysdata");
-      currentdaysdata.textContent = data.current.temp;
-      currentdaysdata.textContent = data.current.wind;
-      currentdaysdata.textContent = data.current.humidty;
       displayWeather();
     });
 }
 
+function displayWeather() {
+  var currentdaysdata = document.getElementById("currentdaysdata");
+
+  var time = weather["dt"] * 1000;
+  var date = new Date(time).toLocaleDateString("en-US");
+  var iconCode = weather["weather"]["0"]["icon"];
+  var desc = weather["weather"]["0"]["description"];
+  var iconURL = "https://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+  var cityname = weather["name"] + " " + date;
+  var citytemp = "Temperature: " + weather["main"]["temp"] + " °F";
+  var citywind = "Wind: " + weather["wind"]["speed"] + " mph";
+  var cityhumidity = "Humidity: " + weather["main"]["humidity"] + " %";
+
+  currentdaysdata.textContent = data.current.temp;
+  currentdaysdata.textContent = data.current.wind;
+  currentdaysdata.textContent = data.current.humidty;
+  weatherIconEl.innerHTML = `<img src=` + iconURL + `>` + " " + desc;
+}
+
 // getApi(requestUrl);
+
+// Don't think I need this
+// var convertLatLon =
+//   "http://api.openweathermap.org/geo/1.0/direct?q=" +
+//   lat +
+//   "&" +
+//   lon +
+//   "&appid=" +
+//   APIKey;
+
+// function getApi(latLon) {
+//   console.log(latLon);
+//   fetch(convertLatLon).then(function (response) {
+//     if (response.ok) {
+//       var city = response.json().then(function (data) {
+//         console.log(data);
+//         getApi(city);
+//       });
+//     }
+//   });
+// }
